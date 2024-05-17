f2b8d920bd246499af40dafb9f9befa8185cdbef
Ryan <coap.ryanmontgomery@gmail.com>
2024-05-17 09:54:51 -0600

    fetch DOM need to revisit

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+// In index.js, there is an empty function, fetchBooks(), that is called when index.html is loaded. To pass this lab, this function should include a fetch request to the Game of Thrones API (https://anapioficeandfire.com/api/booksLinks to an external site.). The returned response should be converted to JSON. Then, it should call the second function, renderBooks(), passing in the JSON-ified data as the argument. To check if you have done this correctly, open up the index.html page of this lab; you should see a list of Game Of Thrones titles on your webpage.
+
+// // // NOTE: The tests in this lab need to access the fetch() request you will create inside fetchBooks(). In order to give them access, write your solution so that fetchBooks() returns the fetch(). This will not change the behavior of your fetch().
+// o convert the response to JSON, you can use the .json() method on the response object. In your code, you can chain another .then() method after the fetch request to handle the response and convert it to JSON. Here's an example of how you can modify code:
+
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     .then((resp) => resp.json())
+//     .then((json) => {
+//       console.log(json); // This will log the JSON data to the console
+//       return json; // Return the JSON data
+//     });
+// } 
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => {
+      renderBooks(json);
+      return json;
+    })
+    .catch((error) => {
+      console.log("Error fetching books:", error);
+    });
 }
 
 function renderBooks(books) {
@@ -12,6 +32,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
